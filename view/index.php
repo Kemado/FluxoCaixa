4c6d680d87dc2ffbb4244e8edad448604ad4cc9d
Ricardo Camargo Favaro <ricardo-favaro@uam.com.br>
2019-10-01 13:31:58 -0300

    adicionar rotas

--- view/index.php
+++ view/index.php
@@ -1,12 +1,14 @@
 <?php
+
 //teste de commit
 	include_once("header.php");
+	include_once("../controller/rotas.php");
 	// include_once("cad_cliente.php");
 	// include_once("cad_update.php");
 	// include_once("excluir_cli.php");
 	// include_once("buscar_cli.php");
 	// include_once("usuario_cad.php");
 	// include_once("usuario_update.php");
-	include_once("tipo_lancamento.php");
+	//include_once("tipo_lancamento.php");
 	include_once("footer.php");
 ?>
